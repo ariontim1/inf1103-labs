563019a12a22859cd61527a5e8af6bc969baa02f
Ariontim <2104313H@student.tp.edu.sg>
2026-09-04 14:45:28 +0800

    activity2

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
-print("-----------------")
+print("=================")
 print("welcome here")
+print("My first post!")
+print("=================")
+
+username = "cool_creator"
+bio = "fun_blogger"
+followers = 100
+
+
+print ("username:", username)
+print ("bio:", bio)
+print ("Followers:", followers)
+
+#activity 3
