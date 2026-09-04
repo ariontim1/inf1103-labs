50c5d4f774476b40620e67140bdd331159bb58c7
Ariontim <2104313H@student.tp.edu.sg>
2026-09-04 15:00:53 +0800

    activity 5

--- hello.py
+++ hello.py
@@ -1,38 +1,54 @@
-print("=================")
-print("welcome here")
-print("My first post!")
-print("=================")
+#activity 1 & 2
+#print("=================")
+#print("welcome here")
+#print("My first post!")
+#print("=================")
 
-username = "cool_creator"
-bio = "fun_blogger"
-followers = 100
+#username = "cool_creator"
+#bio = "fun_blogger"
+#followers = 100
 
 
-print ("username:", username)
-print ("bio:", bio)
-print ("Followers:", followers)
+#print ("username:", username)
+#print ("bio:", bio)
+#print ("Followers:", followers)
 
 #activity 3
 
-followers = 100
+#followers = 100
 
-followers +=50
-print("Day 1:", followers)
+#followers +=50
+#print("Day 1:", followers)
 
-followers +=20
-print("Day 2", followers)
+#followers +=20
+#print("Day 2", followers)
 
-followers -=10
-print("Day 3:", followers)
+#followers -=10
+#print("Day 3:", followers)
 
 #activity 4
 
+#username = input("Enter Username: ")
+#age = input("Enter age:")
+#category = input("Enter Content category")
+
+#print("\nInstagram Profile")
+#print("====================")
+#print("username:", username)
+#print("age:", age)
+#print("Category:" , category)
+
+
+#activity 5
 username = input("Enter Username: ")
-age = input("Enter age:")
-category = input("Enter Content category")
+age = int(input("Enter age: "))
+category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
 print("====================")
-print("username:", username)
-print("age:", age)
-print("Category:" , category)
+print("Username:", username)
+print("Age:", age)
+print("category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
