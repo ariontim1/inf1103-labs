51b5eff2627e64e760fa545413f0c4b2f7b31983
Ariontim <2104313H@student.tp.edu.sg>
2026-09-04 14:47:52 +0800

    activity 3

--- hello.py
+++ hello.py
@@ -13,3 +13,16 @@ print ("bio:", bio)
 print ("Followers:", followers)
 
 #activity 3
+
+followers = 100
+
+followers +=50
+print("Day 1:", followers)
+
+followers +=20
+print("Day 2", followers)
+
+followers -=10
+print("Day 3:", followers)
+
+
